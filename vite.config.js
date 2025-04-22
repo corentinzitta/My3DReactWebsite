80f75d8174179b64eb1fc44d29c3a30eaac3ab65
corentin <zitta.corentin@gmail.com>
2025-04-22 20:31:43 +0200

    Mise à jour de la configuration de base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://corentinzitta.github.io/My3DReactWebsite/",
+  base: "/My3DReactWebsite/",
 })
